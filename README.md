70bdee547c54cbc1fbfc3a1a5429e0a675af6a91
wangsw <itshiwei@aliyun.com>
2024-03-25 10:39:08 +0800

    feat:Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# NuwaCameraSDKExample
+Example Nuwa CameraSDK API (in NuwaSDK)
+
+# `Nuwa Website`
+* NuwaRobotics Website (https://www.nuwarobotics.com/)
+* NuwaRobotics Developer Website (https://dss.nuwarobotics.com/)
+
+NOTICE : Please get NuwaSDK from Nuwarobotics Developer Website
+
+# `Camera SDK Document`
+* NUWA Camera SDK Introduction
+* https://developer-docs.nuwarobotics.com/sdk/NuwaCameraSDK.html
+
+# `Support Robot Product`
+Robot Generation 1 
+* Kebbi(凱比) : Taiwan
+* Danny(小丹) : China
+
+Robot Generation 2
+* Kebbi Air : Taiwan、Chain、Japan
+
+# `Nuwa Camera SDK`
+Nuwa Camera SDK provide basic camera preview and take picture feature and detect face which register on Nuwa Contacts.
+
+Feature List :
+* take picture
+* start\stop preview (floating window)
+* receive recognition result (FACE_DETECTION\FACE_RECOGNITION\FACE_TRACK\OBJ_RECOGNITION)
+
+## 中文翻译
+# NuwaCameraSDK示例
+示例Nuwa CameraSDK API（在NuwaSDK中）
+
+# `诺瓦官网`
+* 诺瓦机器人官网（https://www.nuwarobotics.com/）
+* 诺瓦机器人开发者网站（https://dss.nuwarobotics.com/）
+
+注意：请从诺瓦机器人开发者网站获取NuwaSDK
+
+# `摄像头SDK文档`
+* NUWA摄像头SDK介绍
+* https://developer-docs.nuwarobotics.com/sdk/NuwaCameraSDK.html
+
+# `支持的机器人产品`
+机器人第一代
+* 凯比（Kebbi）：台湾
+* 丹尼（Danny）：中国
+
+机器人第二代
+* 凯比空气（Kebbi Air）：台湾、中国、日本
+
+# `诺瓦摄像头SDK`
+诺瓦摄像头SDK提供基本的摄像头预览和拍照功能，并检测在诺瓦通讯录中注册的人脸。
+
+功能列表：
+* 拍照
+* 启动\停止预览（浮动窗口）
+* 接收识别结果（FACE_DETECTION\FACE_RECOGNITION\FACE_TRACK\OBJ_RECOGNITION）
+
---
+++ app/src/main/java/com/nuwarobotics/camerasdkdemo/MainActivity.java
@@ -0,0 +1,215 @@
+package com.nuwarobotics.camerasdkdemo;
+
+import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.nuwarobotics.lib.visual.model.FaceData;
+import com.nuwarobotics.lib.visual.model.Recognition;
+import com.nuwarobotics.service.camera.common.Constants;
+import com.nuwarobotics.service.camera.common.CsDebug;
+import com.nuwarobotics.service.camera.sdk.CameraSDK;
+import com.nuwarobotics.service.camera.sdk.OutputData;
+import com.nuwarobotics.utils.Debug;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+    private CameraSDK cameraSDK = new CameraSDK(this);
+    private TextView mOutputTextView;
+    private Gson mGson;
+
+    private class FRData {
+        public static final String UNKNOWN_NAME = "other";
+        public int idx;
+        public String conf = null;
+        public String mask = null; //does user wear mask (support on 1.4620.100JP.1 later version)
+        public String name = null; //user name  (if stranger, this value will be "null" or "@#$")
+        public Rect rect = null; //face rect
+        public String age;
+        public String gender;
+        public long faceid;
+
+        public FRData() {
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button bindBtn = (Button) findViewById(R.id.bindBtn);
+        Button unbindBtn = (Button) findViewById(R.id.unbindBtn);
+
+        mOutputTextView = (TextView) findViewById(R.id.outputTextView);
+        bindBtn.setOnClickListener(mOnClickListener);
+        unbindBtn.setOnClickListener(mOnClickListener);
+
+        Intent intent = new Intent();
+        intent.setClassName(Constants.SERVICE_PACKAGENAME, Constants.SERVICE_CLASSNAME);
+        MainActivity.this.startService(intent);
+
+        mGson = new Gson();
+    }
+
+    private Button.OnClickListener mOnClickListener = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.bindBtn: {
+                    CsDebug.logD(TAG, "bindBtn");
+                    //register get which recognition data
+                    cameraSDK.register(mCameraSDKCallback,  Constants.FACE_DETECTION
+                            |Constants.FACE_RECOGNITION
+                            | Constants.OBJ_RECOGNITION
+                            | Constants.FACE_TRACK
+                            ,this.getClass().getPackage().getName());
+
+                    break;
+                }
+                case R.id.unbindBtn: {
+                    CsDebug.logD(TAG, "unbindBtn");
+                    cameraSDK.unregister(this.getClass().getPackage().getName());
+                    break;
+                }
+            }
+        }
+    };
+
+    private CameraSDK.CameraSDKCallback mCameraSDKCallback = new CameraSDK.CameraSDKCallback() {
+
+        @Override
+        public void onConnected(boolean isConnected) {
+            CsDebug.logD(TAG, "onConnected:" + isConnected);
+        }
+
+        @Override
+        public void onOutput(Map<Integer, OutputData> resultMap) {
+            CsDebug.logD(TAG, "onOutput:" + resultMap);
+            final StringBuilder sb = new StringBuilder();
+            for (Integer type : resultMap.keySet()) {
+                OutputData outputData = resultMap.get(type);
+                if (outputData != null && outputData.data != null && !"null".equals(outputData.data)) {
+                    CsDebug.logD(TAG, "onOutput:" + type + ": " + outputData.processTime + "\t" + outputData.data);
+                    sb.append("" + type + ": " + outputData.processTime + "\t" + outputData.data + "\n");
+                    switch(type){
+                        case Constants.FACE_DETECTION :{
+                            //It allow find all face on preview screen, and get all rect.  (but no mask information)
+                            ObjectMapper sMapper = new ObjectMapper();
+                            List<Rect> list = null;
+                            try {
+                                list = sMapper.readValue(outputData.data, new TypeReference<List<Rect>>() {
+                                });
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            if (list != null && !list.isEmpty()) {
+                                for (Rect rect : list) {
+                                    if(rect != null)
+                                        Log.d(TAG,"Got face rect:("+rect.bottom+","+rect.left+","+rect.right+","+rect.top+")");
+                                }
+                            }
+                            break;
+                        }
+                        case Constants.FACE_RECOGNITION :{
+                            //It allow recognition face in center of screen.
+                            //We can get name、face rect、wear mask information、age、etc.....
+                            //Example of get face data
+                            // "@#$" is a special unknown result, we need to ignore it
+                            FRData returnFace = mGson.fromJson(outputData.data, FRData.class);
+                            if(returnFace != null && !"@#$".equals(returnFace.name) && !"null".equals(returnFace.name)){
+                                Log.d(TAG,"Find FaceData user_name:"+returnFace.name );  //return name come from registered on Robot Family.
+                                Log.d(TAG,"user face rect : "+returnFace.rect.toString());
+                                Log.d(TAG,"user wear mask : "+returnFace.mask);
+                                Log.d(TAG,"user age : "+returnFace.age);
+                            }else{
+                                if(returnFace != null)
+                                    Log.d(TAG,"Unknown stranger "+returnFace.name);
+                            }
+                            break;
+                        }
+                        case Constants.FACE_TRACK :
+                            break;
+                        case Constants.OBJ_RECOGNITION : {
+                            float mConfidence = 0.5f;//standard of confidence
+                            ObjectMapper sMapper = new ObjectMapper();
+
+                            List<Recognition> list = null;
+                            try {
+                                list = sMapper.readValue(outputData.data, new TypeReference<List<Recognition>>() {
+                                });
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            Debug.logD(TAG, "onOutput.list = " + list);
+                            if (list != null && !list.isEmpty()) {
+                                String title = null;
+                                double confidence = 0;
+                                //Find best confidence result.
+                                for (Recognition recognition : list) {
+                                    Debug.logD(TAG, "onOutput:" + recognition.title + ", confidence = " + recognition.confidence);
+                                    if (recognition.confidence > confidence) {
+                                        title = recognition.title;
+                                        confidence = recognition.confidence;
+                                    }
+                                }
+                                if (confidence > mConfidence) {
+                                    Bundle bundle = new Bundle();
+                                    bundle.putString("title", title);
+                                    bundle.putDouble("confidence", confidence);
+                                    bundle.putString("frameId", outputData.frameId);
+                                }
+                            }
+                            break;
+                        }
+                    }
+                    //resultMap.containsKey(Constants.FACE_RECOGNITION)
+
+
+
+                }
+            }
+            final String info = sb.toString();
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mOutputTextView.setText(info);
+                }
+            });
+        }
+
+        @Override
+        public void onPictureTaken(String path) {
+            CsDebug.logV(TAG, "image path=" + path);
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraSDK.unregister(this.getPackageName());
+        super.onPause();
+    }
+}
